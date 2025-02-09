11d1092ce35635fd83aba0b7de07171b0faa8ceb
windowcow <bzbzkang@gmail.com>
2025-02-09 15:04:10 +0900

    Initial Commit

---
+++ ReducerTutorial/ReducerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReducerTutorialApp.swift
+//  ReducerTutorial
+//
+//  Created by windowcow on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ReducerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
