2f9a13e0894693b77b74436a1c12ce7416adbe51
windowcow <bzbzkang@gmail.com>
2025-02-14 11:57:38 +0900

    chore: 안 쓰는 player1Score, player2Score 제거

--- ReducerTutorial/Dependencies/SharedReaderKey+.swift
+++ ReducerTutorial/Dependencies/SharedReaderKey+.swift
@@ -12,13 +12,3 @@ extension SharedReaderKey where Self == InMemoryKey<Player> {
       inMemory("currentTurnPlayer")
   }
 }
-
-extension SharedReaderKey where Self == InMemoryKey<Int> {
-    static var player1Score: Self {
-        inMemory("player1Score")
-    }
-    
-    static var player2Score: Self {
-        inMemory("player2Score")
-    }
-}
