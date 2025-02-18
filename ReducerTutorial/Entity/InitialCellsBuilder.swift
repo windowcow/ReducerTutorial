5b155bbecd797db3a9b641363b909d7009f85113
windowcow <bzbzkang@gmail.com>
2025-02-18 17:55:13 +0900

    refactor: Player.nextPlayer() 제거

--- ReducerTutorial/Entity/InitialCellsBuilder.swift
+++ ReducerTutorial/Entity/InitialCellsBuilder.swift
@@ -1,11 +1,11 @@
 import ComposableArchitecture
 
 struct InitialCellsBuilder {
-    static func build(size: Int = 3) -> IdentifiedArrayOf<Cell.State> {
+    static func build(rows: Int = 3, cols: Int = 3) -> IdentifiedArrayOf<Cell.State> {
         var array = IdentifiedArrayOf<Cell.State>()
         
-        for row in 0..<size {
-            for col in 0..<size {
+        for row in 0..<rows {
+            for col in 0..<cols {
                 let cellState = Cell.State(row: row, col: col)
                 array.append(cellState)
             }
